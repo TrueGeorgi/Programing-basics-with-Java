d0cbacd9d2db5b3c0d0e5df381c2fad83eb81e25
TrueGeorgi <110535908+TrueGeorgi@users.noreply.github.com>
2022-11-05 12:44:11 +0100

    Add files via upload

---
+++ Test_99.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Test_99 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = Math.pow(2.0,8.0);
+        System.out.println(a);
+    }
+}
